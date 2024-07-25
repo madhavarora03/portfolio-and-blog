9a5a649f0fdfc644e8801b5b591715a729cb16fe
madhavarora03 <eragon.cube@outlook.com>
2024-07-25 10:15:21 +0530

    Adding Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: "https://some-site.netlify.app",
+  integrations: [tailwind(), icon(), sitemap()],
 });
